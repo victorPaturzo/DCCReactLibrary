523f6e05acb6cb4cd4aff8ba2489f658ae1b3641
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-10 16:46:30 -0500

    Installed Bootstrap. (VPP)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const jsxElement = <h1>Our React App</h1>;
 
-reactDom.render(jsxElement, document.getElementById('root'));
+reactDom.render(<App/>, document.getElementById('root'));
