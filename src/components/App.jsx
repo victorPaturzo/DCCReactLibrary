0eaad644665299e00ef3d1e13a19d01c22b7337d
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-11 11:31:54 -0500

    Created previous button feature for book cycle component. (VPP)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,28 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render() {
         return (
             <div className="container-fluid">
@@ -22,6 +44,7 @@ class App extends Component {
                 <div className= "row">
                     <div className= "col-md-4">
                         {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className= "col-md-4">
                         {/*Display book with the cover here*/}
@@ -30,6 +53,7 @@ class App extends Component {
                     </div>
                     <div className= "col-md-4">
                         {/*Button here to move to the next book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
                 
