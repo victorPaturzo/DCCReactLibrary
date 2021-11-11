e39319a85da660bbf05bdc093fdab00506e0884e
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-11 14:07:31 -0500

    Separated book view display into separate component. (VPP)

--- src/components/BookViewer/BookViewer.jsx
+++ src/components/BookViewer/BookViewer.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
+import Book from '../Book/Book';
 
 const BookViewer= (props) => {
     return (  
         <div className= "row row-spacer">
             <div className= "col-md-4">
-                {/*Button here to move to the previous book viewed*/}
                 <button onClick={props.previousBook}>Previous Book</button>
             </div>
             <div className= "col-md-4">
-                {/*Display book with the cover here*/}
-                <h1>{props.book.title}</h1>
-                <h4>{props.book.author}</h4>
+                <Book book={props.book}/>
             </div>
             <div className= "col-md-4">
-                {/*Button here to move to the next book viewed*/}
                 <button onClick={props.nextBook}>Next Book</button>
         </div>
 </div>);
